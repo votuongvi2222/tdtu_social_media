89b561e97b2b333d8eb197bed9037d101b628658
Vo Tuong Vi <votuongvi2222002@gmail.com>
2021-11-06 23:44:19 +0700

    Add connection to db

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var mongoose = require('mongoose')
+var mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // connect to db
-// var mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tdtusocialmedia');
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tdtusocialmedia');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
