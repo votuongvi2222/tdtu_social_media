ad586e116ac0d20978ea65d62c83f81b638901be
Vo Tuong Vi <votuongvi2222002@gmail.com>
2021-11-06 22:06:04 +0700

    config dependencies

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// connect to db
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tdtusocialmedia');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
