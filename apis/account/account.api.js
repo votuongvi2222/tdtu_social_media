b3f4889b87b00a2a4c07f5ae163f0729c0248286
Vo Tuong Vi <votuongvi2222002@gmail.com>
2021-11-15 03:25:06 +0700

    Update role attribute in account apis

--- apis/account/account.api.js
+++ apis/account/account.api.js
@@ -1,18 +1,26 @@
 var Account = require('../../models/account'),
+    Role = require('../../models/role'),
     bcrypt = require('bcrypt');
 
 var getAccounts = (req, res) => {
     Account.find((error, accounts) => {
         if(error) return res.send(500, 'Error occurred: Database error!');
         return res.json(accounts.map((account) => {
-            return {
-                id: account._id,
-                username: account.username,
-                createdTime: account.createAt,
-                updatedTime: account.updatedAt, 
-                roleId: account.roleId,
-                avatar: account.avatar
-            }
+            Role.findById(account.roleId, (error, role) => {
+                if(error) return res.send(500, 'Error occurred: Database error!');
+                if(!role) return res.send(404, 'Role id is not found!');
+                return res.json({
+                    id: account._id,
+                    username: account.username,
+                    createdTime: account.createAt,
+                    updatedTime: account.updatedAt, 
+                    role: {
+                        id: role._id,
+                        name: role.name
+                    },
+                    avatar: account.avatar
+                })
+            })
         }))
     })
 }
@@ -21,13 +29,20 @@ var getAccountById = (req, res) => {
     Account.findById(req.params.id, (error, account) => {
         if(error) return res.send(500, 'Error occurred: Database error!');
         if(!account) return res.send(404, 'Account id is not found!');
-        return res.json({
-            id: account._id,
-            username: account.username,
-            createdTime: account.createAt,
-            updatedTime: account.updatedAt, 
-            roleId: account.roleId,
-            avatar: account.avatar   
+        Role.findById(account.roleId, (error, role) => {
+            if(error) return res.send(500, 'Error occurred: Database error!');
+            if(!role) return res.send(404, 'Role id is not found!');
+            return res.json({
+                id: account._id,
+                username: account.username,
+                createdTime: account.createAt,
+                updatedTime: account.updatedAt, 
+                role: {
+                    id: role._id,
+                    name: role.name
+                },
+                avatar: account.avatar
+            })
         })
     })
 }
@@ -36,39 +51,53 @@ var putAccountById = (req, res) => {
     Account.findById(req.params.id, (error, account) => {
         if(error) return res.send(500, 'Error occurred: Database error!');
         if(!account) return res.send(404, 'Account id is not found!');
-        account.username = req.body.username;
-        account.hashedPassword = bcrypt.hashSync(req.body.password, 10);
-        account.roleId = req.body.roleId;
-        account.avatar = req.body.avatar;
-        account.save((error, account)  => {
+        Role.findById(req.body.roleId, (error, role) => {
             if(error) return res.send(500, 'Error occurred: Database error!');
-            return res.json({
-                id: account._id,
-                username: account.username,
-                createdTime: account.createAt,
-                updatedTime: account.updatedAt, 
-                roleId: account.roleId,
-                avatar: account.avatar        
+            if(!role) return res.send(404, 'Role id is not found!');
+            account.username = req.body.username;
+            account.hashedPassword = bcrypt.hashSync(req.body.password, 10);
+            account.roleId = req.body.roleId;
+            account.avatar = req.body.avatar;
+            account.save((error, account)  => {
+                if(error) return res.send(500, 'Error occurred: Database error!');
+                return res.json({
+                    id: account._id,
+                    username: account.username,
+                    createdTime: account.createAt,
+                    updatedTime: account.updatedAt, 
+                    role: {
+                        id: role._id,
+                        name: role.name
+                    },
+                    avatar: account.avatar
+                })
             })
         })
     })
 }
 
 var postAccount = (req, res) => {
-    new Account({
-        username: req.body.username,
-        hashedPassword: bcrypt.hashSync(req.body.password, 10),
-        roleId: req.body.roleId,
-        avatar: req.body.avatar
-    }).save((error, account) => {
+    Role.findById(req.body.roleId, (error, role) => {
         if(error) return res.send(500, 'Error occurred: Database error!');
-        return res.json({
-            id: account._id,
-            username: account.username,
-            createdTime: account.createAt,
-            updatedTime: account.updatedAt, 
-            roleId: account.roleId,
-            avatar: account.avatar        
+        if(!role) return res.send(404, 'Role id is not found!');
+        new Account({
+            username: req.body.username,
+            hashedPassword: bcrypt.hashSync(req.body.password, 10),
+            roleId: req.body.roleId,
+            avatar: req.body.avatar
+        }).save((error, account) => {
+            if(error) return res.send(500, 'Error occurred: Database error!');
+            return res.json({
+                id: account._id,
+                username: account.username,
+                createdTime: account.createAt,
+                updatedTime: account.updatedAt, 
+                role: {
+                    id: role._id,
+                    name: role.name
+                },
+                avatar: account.avatar
+            })
         })
     })
 }
@@ -77,15 +106,22 @@ var deleteAccountById = (req, res) => {
     Account.findById(req.params.id, (error, account) => {
         if(error) return res.send(500, 'Error occurred: Database error!');
         if(!account) return res.send(404, 'Account id is not found!');
-        account.delete((error, account)  => {
+        Role.findById(req.body.roleId, (error, role) => {
             if(error) return res.send(500, 'Error occurred: Database error!');
-            return res.json({
-                id: account._id,
-                username: account.username,
-                createdTime: account.createAt,
-                updatedTime: account.updatedAt, 
-                roleId: account.roleId,
-                avatar: account.avatar        
+            if(!role) return res.send(404, 'Role id is not found!');
+            account.delete((error, account)  => {
+                if(error) return res.send(500, 'Error occurred: Database error!');
+                return res.json({
+                    id: account._id,
+                    username: account.username,
+                    createdTime: account.createAt,
+                    updatedTime: account.updatedAt, 
+                    role: {
+                        id: role._id,
+                        name: role.name
+                    },
+                    avatar: account.avatar
+                })
             })
         })
     })
